f2ee2ca7acb344d5734b16677668cbb53a535aa8
ilarioforcherio <ilarioforcherio@gmail.com>
2022-11-14 17:20:26 +0100

    impostazione pagina

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+
+      todoList:[
+        {testo:'comprare cose',done:true},
+        {testo:'pulire cose',done:true},
+      ],
+
+      indice: 0,
+    },
+
+    created(){
+
+    },
+
+    mounted(){
+
+    },
+
+
+    methods: {
+      
+    }
+  })
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
